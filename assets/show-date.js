e69f203853ddf064d4c0c68b926eb781dedf0dd7
Mingchen Xia <61240575+MingchenXia@users.noreply.github.com>
2025-11-21 09:33:08 +0800

    Add files via upload

--- assets/show-date.js
+++ assets/show-date.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const now = new Date();
+  const options = { year: 'numeric', month: 'long', day: 'numeric', weekday: 'long' };
+  const formatted = now.toLocaleDateString('en-US', options);
 
+  const dateDiv = document.getElementById('current-date');
+  if (dateDiv) {
+    dateDiv.textContent = `I have not received the reimbursement for the lunch before this talk until ${formatted}`;
+  }
+});
